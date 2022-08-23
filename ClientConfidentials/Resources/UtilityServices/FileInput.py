36f375ffa670e3a2cd2c0cfc66b463aa4189d209
creamybokeh <84829072+creamybokeh@users.noreply.github.com>
2022-08-22 18:40:52 -0600

    Added code to the file input and output modules.

--- ClientConfidentials/Resources/UtilityServices/FileInput.py
+++ ClientConfidentials/Resources/UtilityServices/FileInput.py
@@ -1,1 +1,12 @@
-Import API Dataset
+# Import API Dataset
+
+import csv
+
+def load_csv(csvpath):
+    with open(csvpath, "r") as csv_file:
+        data = []
+        csvreader = csv.reader(csv_file, delimiter = ",")
+        for row in csvreader:
+            data.append(row)
+
+    return data
--- ClientConfidentials/Resources/UtilityServices/FileOutput.py
+++ ClientConfidentials/Resources/UtilityServices/FileOutput.py
@@ -1,1 +1,15 @@
 #Export to CSV
+import csv 
+
+def save_as_csv(csvpath):
+
+# csv_path = Path('stock_data_output.csv')
+with open(csvpath, 'w', newline='') as csv_file:
+    header = []
+    data =[]
+    csvwriter = csv.writer(csv_file, delimiter = ",")
+
+    csvwriter.writerow(header)
+    csvwriter.writerow(data)
+
+return csvwriter
