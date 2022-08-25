78401d47f8ed06c7ae4f29d2405759c29d04f3c2
Tyler Hillison <thillison@gmail.com>
2022-08-25 16:02:11 -0700

    Updated the app to take both tickers and number of shares as inputs.

--- ClientConfidentials/app.py
+++ ClientConfidentials/app.py
@@ -1,4 +1,3 @@
-# Import libraries
 from pathlib import Path
 import csv
 import sys
@@ -9,7 +8,7 @@ import questionary
 def run():
 
     # set csv file path
-    csvpath = Path("..//ClientConfidentials/Resources/client_tickers.csv")
+    csvpath = Path("ClientConfidentials/Resources/client_tickers.csv")
 
     # Portfolio Tickers 
     portfolio_tickers = [] 
@@ -20,16 +19,18 @@ def run():
     while value == 'yes':
         client_ticker = questionary.text("Would you please enter the Ticker Symbol of your Portfolio Holding?").ask()
         client_ticker = str(client_ticker)
-        portfolio_tickers.append(client_ticker)
+        share_amount = questionary.text("What is the number of shares you hold in this stock?").ask()
+        share_amount = str(share_amount)
+        portfolio_tickers.append([client_ticker,share_amount])
 
             # Write Client Tickers csv file Output
-        with open(csvpath, 'w', encoding = 'UTF-8') as csvfile:
+        with open(csvpath, 'w', encoding = 'UTF-8',newline='') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=',')
             csvwriter.writerow(csvheader)
 
         # Iterate tickers for csv file output   
             for ticker in portfolio_tickers:
-                csvwriter.writerow([ticker])
+                csvwriter.writerow([ticker[0],ticker[1]])
 
         # Prompt User to Continue or Not
         value = questionary.text("Would you like to enter another Ticker from your Portfolio? Type: yes or no").ask()
